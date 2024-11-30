244963ff8b0473ce7d95b7f9e8afa72bc583ba78
VictorJunqueira1 <victorjunqueira.prog@gmail.com>
2024-11-30 01:52:53 -0300

    [fix] Deploy error, carregando as variáveis de ambiente.

--- src/cache/redisService.ts
+++ src/cache/redisService.ts
@@ -1,12 +1,15 @@
 import Redis from 'ioredis';
-
+import dotenv from 'dotenv';
 export class RedisService {
     private client: Redis;
 
     constructor() {
+        dotenv.config();
+
         this.client = new Redis({
             host: process.env.REDIS_HOST || '127.0.0.1',
             port: Number(process.env.REDIS_PORT) || 6379,
+            password: process.env.REDIS_PASSWORD || undefined,
         });
 
         this.client.on('connect', () => {
